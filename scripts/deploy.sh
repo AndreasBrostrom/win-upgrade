f037ab02da178439ea7ce5c8e013f9ad61ef934b
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-03-23 19:00:24 +0100

    Think this works

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,12 @@
-#/bin/bash
+#!/bin/bash
 
-echo "TODO"
+set -e
+
+VERSION_TAG=$*
+
+mkdir release/
+
+sed -i "s/*BUILD =.*/BUILD =. \"${VERSION_TAG}\"/" release/upgrade.ps1
+
+# Pack Compositions
+zip release/upgrade-${VERSION_TAG}.zip upgrade.cmd upgrade.ps1 README.md
