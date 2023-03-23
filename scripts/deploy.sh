703ec0f28fd8863e23b734f0faae245e2ab1ed65
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2023-03-23 19:08:31 +0100

    Fixed deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@ set -e
 
 VERSION_TAG=$*
 
-mkdir release/
+mkdir -p release/
 
-sed -i "s/*BUILD =.*/BUILD =. \"${VERSION_TAG}\"/" release/upgrade.ps1
+sed -i "s/\$BUILD = \"DEV\"*/\$BUILD = \"${VERSION_TAG}\"/" upgrade.ps1
 
 # Pack Compositions
 zip release/upgrade-${VERSION_TAG}.zip upgrade.cmd upgrade.ps1 README.md
