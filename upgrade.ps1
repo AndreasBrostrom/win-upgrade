5694fe778179d6e91d79d736694f07b74395b770
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-04-02 12:51:50 +0200

    upgrade

--- upgrade.ps1
+++ upgrade.ps1
@@ -168,7 +168,7 @@ function runWSLUpdate {
             if (-not $suMode) {
                 $distPackageManagers = (
                     "eval '" +
-                    "type paru > /dev/null 2>&1; &&" +
+                    "type paru > /dev/null 2>&1 &&" +
                     "  echo -e `"\033[1;32mparu\033[0m`" &&" +
                     "  yes `"`" | paru -Syyu --sudoloop --noconfirm --color=always &&" +
                     "  exit `$?;" +
@@ -181,11 +181,11 @@ function runWSLUpdate {
                 $distPackageManagers = (
                     "eval '" +
                     "sudo -v;" +
-                    "type yay > /dev/null 2>&1; &&" +
+                    "type yay > /dev/null 2>&1 &&" +
                     "  echo -e `"\033[1;32myay\033[0m`" &&" +
                     "  yes `"`" | yay -Syyu --sudoloop --noconfirm --color=always &&" +
                     "  exit `$?;" +
-                    "type paru > /dev/null 2>&1; &&" +
+                    "type paru > /dev/null 2>&1 &&" +
                     "  echo -e `"\033[1;32mparu\033[0m`" &&" +
                     "  yes `"`" | paru -Syyu --sudoloop --noconfirm --color=always &&" +
                     "  exit `$?;" +
